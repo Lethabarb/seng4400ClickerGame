055827f0e91b83e2e0fe9748eec2463d32e89ae8
Lethabarb <colininja@outlook.com.au>
2024-06-05 17:09:17 +1000

    removed keys

--- GetLeaderboard/Function.cs
+++ GetLeaderboard/Function.cs
@@ -14,7 +14,7 @@ namespace GetLeaderboard;
 
 public class Function
 {
-    private static readonly AmazonDynamoDBClient dynamoDbClient = new AmazonDynamoDBClient("ASIAVURCVOKB64J5VIMT", "ILlbQHh7nqfJYbFIoSSJZjPS+gZA6mKxWtHrJW/h", "IQoJb3JpZ2luX2VjEC0aCXVzLXdlc3QtMiJHMEUCIQCpk8teUVc4dgaP4w0/mf13P07QIMcXI6MpF60BwyN51wIgWRbA1iXZmjOwGjoWPJKnP3Y2JlVmjrW1kzAuVhNDU0wqsQIItv//////////ARABGgwzODc2OTM1MDcyMDMiDMW2mWpgITaoK76WECqFAo7MyI2dvaJlWGRFmV3HSzR167aVKMwhqFA5GUuU7aR/atJVLxQpf7ruhGxtsms17bkw6BASr2xGyUDMrk1ixa5jPeaJaS1AQ3KmRTklC2zq2Rd/91x1MKi2zAGpCje5TjaCrnNMU9tGPljp3cvl+qbMknaexZuKoJoqKqT8scfZC1Vlr6bPDTQmjOo6R7Ew64M7dU3sEQg151n3Dau0FDHUUoUUtv2UNqBfq4Nhb8etuWvCY0yuVpwOgMhD3a/UZMtE4vlYeMTNDUzE7KSIgik4K0+KNxJsAddY7G48OdHJVVovCJhwIdsmPNuhRYBCs9dxh/IQV4puT7Y1zgY4aLvMfF3wBTDz6f+yBjqdAVBd4DHQVNix+gS1qmPG4tYoG7S3VOYj7kqe1pBq9AhlAJeNQK9LLJR/1XTR7ap3G1Zr5E5L4XYO/5Uiblex5BjakLQzKOklB1JiGv4wLGkbk48OUl8vh0UVrpWRhZIiyQY+3MuYQHyIPcQiandurz3rvck2ieXwxGybKrKR81IeUZj4NByMRvFEMNMCN+LDJevvtEKIS80hXWFvMMU=", RegionEndpoint.USEast1);
+    private static readonly AmazonDynamoDBClient dynamoDbClient = new AmazonDynamoDBClient();
     private static readonly string tableName = "PlayerSessions"; // Replace with your DynamoDB table name
 
     public async Task<List<LeaderboardPosition>> FunctionHandler(ILambdaContext context)
