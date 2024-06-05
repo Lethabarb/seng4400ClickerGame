0ddaebae49ecb447c9b24728307a31d59d3be851
Lethabarb <colininja@outlook.com.au>
2024-06-05 16:33:00 +1000

    Process Leaderboard function

---
+++ Frontend/Models/ScoreComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+
+namespace Frontend.Models
+{
+    public class ScoreComparer : IComparer<LeaderboardPosition>
+    {
+
+        public int Compare(LeaderboardPosition? x, LeaderboardPosition? y)
+        {
+            if (x.Score > y.Score) return 1;
+            if (x.Score < y.Score) return -1;
+            return 0;
+        }
+    }
+}
--- GetLeaderboard/Function.cs
+++ GetLeaderboard/Function.cs
@@ -1,5 +1,8 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.Core;
-using Amazon.Lambda.DynamoDBEvents;
+using Frontend.Models;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -8,18 +11,16 @@ namespace GetLeaderboard;
 
 public class Function
 {
-    public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+    private static readonly AmazonDynamoDBClient dynamoDbClient = new AmazonDynamoDBClient();
+    private static readonly string tableName = "PlayerSessions"; // Replace with your DynamoDB table name
+    public async Task<List<LeaderboardPosition>> FunctionHandler(ILambdaContext context)
     {
-        context.Logger.LogInformation($"Beginning to process {dynamoEvent.Records.Count} records...");
-
-        foreach (var record in dynamoEvent.Records)
-        {
-            context.Logger.LogInformation($"Event ID: {record.EventID}");
-            context.Logger.LogInformation($"Event Name: {record.EventName}");
-            
-            // TODO: Add business logic processing the record.Dynamodb object.
-        }
-
-        context.Logger.LogInformation("Stream processing complete.");
+        List<string> attributes = new List<string> { "name", "Score" };
+        ScanResponse res = await dynamoDbClient.ScanAsync(tableName, attributes);
+        string output = JsonConvert.SerializeObject(res.Items);
+        List<LeaderboardPosition> positions = JsonConvert.DeserializeObject<List<LeaderboardPosition>>(output);
+        positions.Sort(new ScoreComparer());
+        List<LeaderboardPosition> top10 = positions.GetRange(0, 10);
+        return top10;
     }
 }
